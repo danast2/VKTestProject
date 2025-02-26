19986da2da2736e3f0ee93f916a1e688baa71f52
Daniil <danast_2@mail.ru>
2025-02-26 22:01:02 +0300

    first

---
+++ VKTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKTestProject
+//
+//  Created by Даниил Асташов on 26.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
